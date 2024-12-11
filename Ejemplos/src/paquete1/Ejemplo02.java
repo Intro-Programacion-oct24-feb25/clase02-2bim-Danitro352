f25a72e5bc5f547745b0599fa33beaf7e38d2b6e
Daniel Idrovo <doidrovo@utpl.edu.ec>
2024-12-11 13:05:17 -0500

    cambios1

--- Ejemplos/src/paquete1/Ejemplo02.java
+++ Ejemplos/src/paquete1/Ejemplo02.java
@@ -18,24 +18,24 @@ public class Ejemplo02 {
         // Declarar y generar arreglos en Java
 
         // arreglo de tipo entero
-        int[] arreglo1 = new int[5];
-        System.out.println(arreglo1[0]);
+        int[] arreglo1 = new int[5]; 
+        System.out.println(arreglo1[0]); 
         System.out.println(arreglo1[1]);
         System.out.println(arreglo1[2]);
         System.out.println(arreglo1[3]);
-        System.out.println(arreglo1[4]);
+        System.out.println(arreglo1[4]); //imprime 0 en todos porque no se le ha dado ningun valor
         
         // arreglo de tipo double
         double[] arreglo2 = new double[5];
-        System.out.println(arreglo2[0]);
+        System.out.println(arreglo2[0]); //imprime 0.0 porque es double y no se le ha dado un valor al arreglo
         
         // arreglo de tipo String
         String[] arreglo4 = new String[5];
-        System.out.println(arreglo4[0]);
+        System.out.println(arreglo4[0]);//imprime null porque es de tipo cadena y no tiene ningun valor
         
         // arreglo de tipo boleano
         boolean[] arreglo5 = new boolean[5];
-        System.out.println(arreglo5[0]);
+        System.out.println(arreglo5[0]); //imprime false porque es de tipo booleano y no tiene ningun valor
     }
 
 }
